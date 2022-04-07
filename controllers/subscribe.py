4cf71f70deda72803610b02c981335811d57b016
Casey Johnson <33426783+caseyjohnsonwv@users.noreply.github.com>
2022-04-07 19:56:17 -0400

    Twilio SMS reply works when deployed on EC2 (#1)
    * try to get around 422
    
    * try loading json
    
    * swap json for body
    
    * try something else
    
    * ditch request, try body
    
    * try literally anything
    
    * async???
    
    * idk
    
    * use the right function
    
    * copy code straight from twilio
    
    * that worked somehow
    
    * message tweaks
    
    * message tweak 2: electric boogaloo
    
    * plan to build infra
    
    Co-authored-by: Casey Johnson <caseyjohnsonwv@gmail.com>

--- controllers/subscribe.py
+++ controllers/subscribe.py
@@ -1,46 +1,18 @@
 import time
 import uuid
-from fastapi import APIRouter
+from fastapi import APIRouter, Form, Response
 from utils.aws import DynamoDB
 from utils.sms import reply_to_sms
 import utils.nlp as nlp
-from pydantic import BaseModel
 
 
 router = APIRouter(prefix='/alerts')
 
 
-class AlertCreationRequest(BaseModel):
-    phone_number:str
-    message_body:str
-
-
-@router.post('/create', status_code=201)
-def create_alert(req:AlertCreationRequest):
-    park = nlp.extract_park(req.message_body)
-    ride = nlp.extract_ride(req.message_body, park.park_id)
-    wait_time = nlp.extract_wait_time(req.message_body)
-    alert = DynamoDB.AlertRecord(str(uuid.uuid4()), req.phone_number, park.park_id, ride.ride_id, wait_time, time.time(), time.time()+7200)
-    alert.write_to_dynamo()
-    return {'alert':str(alert)}
-
-
-class TwilioMessageRequest(BaseModel):
-    MessageSid: str
-    SmsSid: str
-    AccountSid: str
-    MessagingServiceSid: str
-    From: str
-    To: str
-    Body: str
-    NumMedia: int
-    ReferralNumMedia: int
-
-
 @router.post('/twilio')
-def sms_reply(req:TwilioMessageRequest):
-    msg = req.Body
-    phone_number = req.From
+async def sms_reply(From: str = Form(...), Body: str = Form(...)):
+    msg = Body
+    phone_number = From
 
     alert_id = str(uuid.uuid4())
     park = nlp.extract_park(msg)
@@ -60,5 +32,6 @@ def sms_reply(req:TwilioMessageRequest):
     )
     alert.write_to_dynamo()
 
-    reply = f"Alert created! I'll watch https://queue-times.com/ for a wait under {wait_time} minutes on {ride.ride_name}."
-    return reply_to_sms([reply])
+    reply = f"Alert created! I'll watch for a wait under {wait_time} minutes on {ride.ride_name}. Powered by https://queue-times.com/"
+    response = reply_to_sms([reply])
+    return Response(content=str(response), media_type="application/xml")
---
+++ infrastructure/runtime.tf
@@ -0,0 +1,9 @@
+# VPC
+
+# SUBNET
+
+# SECURITY GROUP
+
+# EC2
+
+# install stuff on EC2 instance?
