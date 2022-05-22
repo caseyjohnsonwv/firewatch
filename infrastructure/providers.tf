0af235e241a399e93a2b6510254156c7c700817a
Casey Johnson <caseyjohnsonwv@gmail.com>
2022-05-22 17:17:36 -0400

    terraform fmt go brr

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,12 +1,12 @@
 terraform {
-    required_providers {
-        heroku = {
-            source  = "heroku/heroku"
-            version = "~> 5.0"
-        }
-        null = {
-            source = "hashicorp/null"
-            version = "~> 3.0"
-        }
+  required_providers {
+    heroku = {
+      source  = "heroku/heroku"
+      version = "~> 5.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,18 +1,18 @@
 variable "env_name" {
-    type = string
+  type = string
 }
 
 variable "twilio_account_sid" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "twilio_auth_token" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "twilio_phone_number" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
